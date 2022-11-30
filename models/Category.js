76a08c9ee5cdfc5f38f6209163d959d31e786b99
wwartick <willwartick@gmail.com>
2022-11-29 17:42:05 -0800

    filled out models

--- models/Category.js
+++ models/Category.js
@@ -5,26 +5,26 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    category_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
 );
 
 module.exports = Category;
